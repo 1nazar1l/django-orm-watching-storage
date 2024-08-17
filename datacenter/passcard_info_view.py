48b8a83322551dcf30632eb33519bf2cbe1ff7dd
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-08-17 17:07:10 +0300

    make code more clear

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,40 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from tools import get_duration, format_duration
 
+
+def is_visit_long(delta):
+        total_seconds = delta.total_seconds()
+        hours_inside = total_seconds // 3600
+        result = hours_inside >= 1
+        return result
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    passcard = get_object_or_404(Passcard, passcode=passcode)
+    passcards_visits = Visit.objects.filter(passcard = passcard)
+    this_passcard_visits  = []
+    
+    for passcard_visit in passcards_visits:
+        
+        leaved_at = passcard_visit.leaved_at
+        entered_at = passcard_visit.entered_at
+        
+        time_inside_bank = get_duration(entered_at, leaved_at)
+        suspicious_visit = is_visit_long(time_inside_bank)
+        formatted_time_inside = format_duration(time_inside_bank)
+        
+        this_passcard_visits.append(
+            {
+                'entered_at': entered_at,
+                'duration': formatted_time_inside,
+                'is_strange': suspicious_visit
+            })
+        
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
     }
+    
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,28 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from tools import get_duration, format_duration
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    not_leaved_passcards = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    
+    for passcard in not_leaved_passcards:
+        entered_at = passcard.entered_at
+        
+        time_inside_bank = get_duration(entered_at)
+        formatted_time_inside = format_duration(time_inside_bank)
+        
+        non_closed_visits = [
+            {
+                'who_entered': passcard.passcard.owner_name,
+                'entered_at': entered_at,
+                'duration': formatted_time_inside,
+            }
+        ]
+        
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits, 
     }
+    
     return render(request, 'storage_information.html', context)
